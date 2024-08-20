783dc2b95531893cd5fca457ece5e0ab3a7b84a5
PaterDev <josepater_1130@hotmail.com>
2024-08-19 19:09:27 -0500

    feat: added note buttons that change the key

--- app/src/main/java/com/josepaternina/idnotas/MainActivity.kt
+++ app/src/main/java/com/josepaternina/idnotas/MainActivity.kt
@@ -5,68 +5,155 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.*
+import androidx.compose.ui.text.style.TextAlign
+
+// Tonalidades ("C", "D", "E", "F", "G", "A", "B")
+val tonalidades = listOf("C", "D", "G")
+val num = listOf(1, 2, 3, 4, 5, 6)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RandomNoteApp()
+            MyApp()
         }
     }
 }
 
 @Composable
-fun RandomNoteApp() {
-    // Define el array de notas ("C", "D", "E", "F", "G", "A", "B")
-    val tonalidades = arrayListOf("C", "D", "G")
-
-    // Estado para almacenar la nota actual
-    var tonalidadActual by remember { mutableStateOf(tonalidades.random()) }
-
-    // Función para cambiar la nota aleatoriamente
-    fun cambiarTonalidad() {
-        tonalidadActual = tonalidades.random()
-    }
-
+fun MyApp() {
     // UI
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(1.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Tonalidad: $tonalidadActual",
-            fontSize = 24.sp
+            text = "Tonalidad: ${GlobalStateTonal.tonalidadActual}",
+            fontSize = 22.sp
+        )
+        Text(
+            text = "# ${GlobalStateTonal.numActual}",
+            fontSize = 20.sp
         )
 
         Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = { cambiarTonalidad() }) {
-            Text(text = "Nueva tonalidad")
-        }
+        NoteButtonsRow()
     }
 }
 
+// Singleton object que almacena la tonalidad actual
+object GlobalStateTonal {
+    // variable global reactiva
+    var tonalidadActual by mutableStateOf(tonalidades.random())
+    var numActual by mutableIntStateOf(num.random())
+
+    // Función para cambiar la tonalidad aleatoriamente
+    fun cambiarTonalidad() {
+        tonalidadActual = tonalidades.random()
+        numActual = num.random()
+    }
+}
+
+// Botones
+@Composable
+fun NoteButtonsRow() {
+    // Define el array de nombres de botones
+    val notasMayores = arrayListOf("C", "D", "E", "F", "G", "A", "B")
+    val notasMenores = arrayListOf("Cm", "Dm", "Em", "Fm", "Gm", "Am", "Bm")
+    val notasMenoresSos = arrayListOf("C#m", "D#m", "F#m", "G#m", "A#m")
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //Notas Mayores
+        Row {
+            notasMayores.forEach { nota ->
+                Button(
+                    onClick = {
+                        GlobalStateTonal.cambiarTonalidad()
+                    },
+                    modifier = Modifier
+                        .width(54.dp) // Establece el ancho del botón
+                        .height(54.dp) // Establece la altura del botón
+                        .padding(2.dp) // Aplica el margen interno del botón
+                ) {
+                    Text(
+                        text = nota,
+                        fontSize = 14.sp,
+                        textAlign = TextAlign.Center // Centra el texto horizontalmente
+                    )
+                }
+            }
+        }
+
+        //Notas menores
+        Row {
+            notasMenores.forEach { nota ->
+                FilledTonalButton(
+                    onClick = {
+                        GlobalStateTonal.cambiarTonalidad()
+                    },
+                    modifier = Modifier
+                        .width(56.dp) // Establece el ancho del botón
+                        .height(56.dp) // Establece la altura del botón
+                        .padding(2.dp) // Aplica el margen interno del botón
+                ) {
+                    Text(
+                        text = nota,
+                        fontSize = 10.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+
+        // Notas menores #
+        Row(
+            horizontalArrangement = Arrangement.Center // Espacia uniformemente los botones
+        ) {
+            notasMenoresSos.forEach { nota ->
+                OutlinedButton(
+                    onClick = {
+                        GlobalStateTonal.cambiarTonalidad()
+                    },
+                    modifier = Modifier.padding(horizontal = 1.dp)
+                ) {
+                    Text(
+                        text = nota,
+                        fontSize = 12.sp, // Tamaño del texto
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+
+}
 
 @Preview(showSystemUi = true)
 @Composable
 fun Preview() {
-    RandomNoteApp()
+    MyApp()
 }
